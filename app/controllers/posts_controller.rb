a463ddf7e9f22301c300fdb69fe5ca3674772140
Mike McCready <michaeljmccready@gmail.com>
2015-07-06 00:48:31 -0700

    current user id populates post user_id upon create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,6 +24,7 @@ class PostsController < ApplicationController
   # POST /posts.json
   def create
     @post = Post.new(post_params)
+    @post.user_id = current_user.id
 
     respond_to do |format|
       if @post.save
