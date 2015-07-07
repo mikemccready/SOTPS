5e3a65d859bad4b4d0db8ff173f3384583df1572
Mike McCready <michaeljmccready@gmail.com>
2015-07-06 18:41:23 -0700

    reformats comments -- adds user name to comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -19,7 +19,7 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.new(comment_params)
-    @comment.user_id = current_user.id
+    @comment.user = current_user
 
     respond_to do |format|
       if @comment.save
