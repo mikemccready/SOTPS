49bbb8c9b46e125d05ae5a4587ab6c002db49031
Mike McCready <michaeljmccready@gmail.com>
2015-07-06 19:40:05 -0700

    adds vote model associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ActiveRecord::Base
 	
 	belongs_to :user
 	has_many :comments
+	has_many :votes, as: :voteable
 
 	has_attached_file :image, styles: { thumb: "32x32", small: "64x64", med: "100x100", large: "200x200", banner: "1200x300" }
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
