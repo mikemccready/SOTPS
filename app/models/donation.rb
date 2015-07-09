149d430fc01f63000a1d201f9560151811c5b8f9
Mike McCready <michaeljmccready@gmail.com>
2015-07-09 16:39:03 -0700

    adds donation model associations

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,2 +1,6 @@
 class Donation < ActiveRecord::Base
+
+	belongs_to :user
+	belongs_to :post
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ActiveRecord::Base
 	belongs_to :category
 	has_many :comments
 	has_many :votes, as: :voteable
+	has_many :donations
 
 	has_attached_file :image, styles: { thumb: "32x32", small: "64x64", med: "100x100", large: "200x200", banner: "1200x300" }
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
