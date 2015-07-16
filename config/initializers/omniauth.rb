2eb50a8cb03ab1e89a5d665eec3eca0e23fad52d
Mike McCready <michaeljmccready@gmail.com>
2015-07-16 02:34:40 -0700

    formatting headers

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['facebook_client_id'], ENV['facebook_secret']
+  provider :facebook, ENV['facebook_client_id'], ENV['facebook_secret'], :image_size => 'large'
 end
