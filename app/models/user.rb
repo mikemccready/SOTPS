564736b033289c577f789af23190c1e88099e495
Mike McCready <michaeljmccready@gmail.com>
2015-07-17 02:31:39 -0700

    formats user show adds user_funds method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,5 +20,10 @@ class User < ActiveRecord::Base
   	def total_user_votes
   		self.posts.map{|a| a.total_votes.to_i}.sum	
   	end
+
+  	def user_funds
+		# self.donations.sum(:amount)
+		self.posts.map{|a| a.donations.amount.to_i}.sum/100
+	end	
     
 end
